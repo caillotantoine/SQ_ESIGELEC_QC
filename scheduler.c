d73b4d4f76402b0542cc7e4314ee487a44239752
Antoine <ant.kyo@gmail.com>
2018-03-08 17:08:47 +0100

    M3 | Add comments

--- scheduler.c
+++ scheduler.c
@@ -7,7 +7,7 @@
 #include <msp430g2553.h>
 #include "scheduler.h"
 
-uint16_t Tick_out=0; /* MUST DECLARE AND INSTANTIATE HERE */
+uint16_t Tick_out=0;                /* The variable is now local to the module */
 
 /* Called from TA_0 ISR*/
 void callback_2ms(void) 
@@ -17,20 +17,20 @@ void callback_2ms(void)
 
 uint8_t check_and_clear_Tick_out(void)
 {
-    uint16_t timer_backup = 0;
-    uint8_t  tckout = 0;
+    uint16_t timer_backup = 0;      /* Save of the timer state */
+    uint8_t  tckout = 0;            /* Variable used to return the value of Tick_out */
 
-    __disable_interrupt();
+    __disable_interrupt();          /* We disable the interruptions to avoid race conditions */
 
-    timer_backup = TA0CTL & TAIE;
-    TA0CTL &= ~TAIE;
-    tckout = Tick_out;
-    Tick_out = 0;
-    TA0CTL |= timer_backup;
+    timer_backup = TA0CTL & TAIE;   /* We save the state of the timer interruption */
+    TA0CTL &= ~TAIE;                /* Reset the timer interruption */
+    tckout = Tick_out;              /* We save the Tick_out value to return it later */
+    Tick_out = 0;                   /* We reset Tick_out */
+    TA0CTL |= timer_backup;         /* We reset the timer interruption to the previous value*/
 
-    __enable_interrupt();
+    __enable_interrupt();           /* All the work on the variable is done, we can re-enable the interruption system */
 
-    return tckout;
+    return tckout;                  /* We send the value of Tick_out */
 }
 
 
