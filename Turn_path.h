ae72d6dfd6b21c71f4d919a64492c2517651d428
Antoine <ant.kyo@gmail.com>
2018-03-21 15:52:37 +0100

    M12 | Working fine, wait for pending..

--- Turn_path.h
+++ Turn_path.h
@@ -14,6 +14,9 @@
 
 int8_t turn_step_basic(uint8_t direction, int8_t innerWspeed, int8_t outerWspeed, uint16_t outerWturns, uint8_t outerWsteps);
 int8_t turn_step_plus(uint8_t runDirection, uint8_t direction, uint8_t innerWspeed, uint8_t outerWspeed, uint16_t outerWturns, uint8_t outerWsteps);
-uint16_t distance2steps(distance_type distance);
+uint32_t distance2steps(distance_type distance);
+int8_t check_stops_turning();
+int8_t turn_step_basic_nb_start(uint8_t direction, int8_t innerWspeed, int8_t outerWspeed, uint16_t outerWturns, uint8_t outerWsteps);
+int8_t turn_step_plus_nb(uint8_t runDirection, uint8_t direction, uint8_t innerWspeed, uint8_t outerWspeed, uint16_t outerWturns, uint8_t outerWsteps);
 
 #endif /* SQ_ESIGELEC_QC_TURN_PATH_H_ */
